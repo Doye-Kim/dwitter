a0f331bf3c06c51ce4e0de9efb1266b6440464b5
도예 <kmdy125@gmail.com>
2023-10-17 23:01:40 +0900

    Feat: upload dweet

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,10 +1,35 @@
 import React from "react";
-
+import { useState } from "react";
+import { dbService } from "../fbase";
 const Home = () => {
+  const [dweet, setDweet] = useState("");
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    await dbService.collection("dweets").add({
+      dweet,
+      createdAt: Date.now(),
+    });
+    setDweet("");
+  };
+  const onChange = (e) => {
+    const {
+      target: { value },
+    } = e;
+    setDweet(value);
+  };
   return (
-    <>
-      <span>Home</span>
-    </>
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          type="text"
+          value={dweet}
+          onChange={onChange}
+          placeholder="what's on your mind?"
+          maxLength={120}
+        />
+        <input type="submit" value="Dweet" />
+      </form>
+    </div>
   );
 };
 export default Home;
