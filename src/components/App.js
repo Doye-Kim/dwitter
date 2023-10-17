f31912ebcf7b9fdf7f1a604681cbd9635f203858
도예 <kmdy125@gmail.com>
2023-10-17 16:36:25 +0900

    feat: login with email, google, github

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,24 @@
 import AppRouter from "components/Router";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { authService } from "fbase";
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(authService.currentUser);
+  const [init, setInit] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  useEffect(() => {
+    authService.onAuthStateChanged((user) => {
+      if (user) {
+        setIsLoggedIn(true);
+      }
+      if (!user) {
+        setIsLoggedIn(false);
+      }
+      setInit(true);
+    });
+  }, []);
   return (
     <>
-      <AppRouter isLoggedIn={isLoggedIn} />
+      {init ? <AppRouter isLoggedIn={isLoggedIn} /> : "Initializing.."}
       <footer>&copy; dwitter {new Date().getFullYear()}</footer>
     </>
   );
