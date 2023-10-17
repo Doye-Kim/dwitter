55b2f3916989fb446eaca5e52e3f964f53524df6
도예 <kmdy125@gmail.com>
2023-10-17 23:20:00 +0900

    Feat: read dweets

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,8 +1,23 @@
 import React from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { dbService } from "../fbase";
 const Home = () => {
   const [dweet, setDweet] = useState("");
+  const [dweets, setDweets] = useState([]);
+  const getDweets = async () => {
+    const dbDweets = await dbService.collection("dweets").get();
+    dbDweets.forEach((document) => {
+      const dweetObject = {
+        ...document.data(),
+        id: document.id,
+      };
+      setDweets((prev) => [dweetObject, ...prev]);
+    });
+    console.log(dweets);
+  };
+  useEffect(() => {
+    getDweets();
+  }, []);
   const onSubmit = async (e) => {
     e.preventDefault();
     await dbService.collection("dweets").add({
@@ -29,6 +44,13 @@ const Home = () => {
         />
         <input type="submit" value="Dweet" />
       </form>
+      <div>
+        {dweets.map((dweet) => (
+          <div key={dweet.id}>
+            <h4>{dweet.dweet}</h4>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
